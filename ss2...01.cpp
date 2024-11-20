5b9eb3f21ee3c895ae1ad9460ba7bb2efdbfb0ed
giahung123456 <dogiahungtplc@gmail.com>
2024-11-20 23:07:08 +0700

    UPLOAD SS2.__01

---
+++ ss2...01.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ main(){
+ 	// Kieu du lieu int: Dung de luu tru so nguyen 
+	 int a=3;// Khai bao bien a kieu int va gan gia tri 3
+ 	printf("%d\n",a);
+ 	// Kieu du lieu float: Dung de luu tru so thuc 
+	 float b=2.5;// Khai bao bien b kieu float va gan gia tri 2.5
+ 		printf("%f\n",b);
+ 	// Kieu du lieu char: Dung de luu tru mot ky tu don 
+	 char c='d';// Khai bao bien c kieu char và gan ky tu 'd'
+ 		printf("%c\n",c);
+ 	// Kieu du lieu int: Dung de luu tru gia tri true ho?c false. neu true thi gia tri la 1, con false gia tri la 0
+	 int t=a<b;//Khai bao bien t kieu int và gan gia tri 0
+ 		printf("%d\n",t);
+ }
